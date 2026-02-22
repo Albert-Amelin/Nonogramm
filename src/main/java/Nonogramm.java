9934871cd395ab25fda264a8e1302c54b2dfc720
Albert Amelin <albert.amelin@gmx.de>
2026-02-22 20:33:45 +0100

    feat: Added attributes of Nonogramm class.

--- src/main/java/Nonogramm.java
+++ src/main/java/Nonogramm.java
@@ -1,2 +1,9 @@
+import java.util.*;
+
 public class Nonogramm {
+    private final int rows = 10;
+    private final int cols = 10;
+    private ArrayList<ArrayList<Integer>> rowSequences;
+    private ArrayList<ArrayList<Integer>> colSequences;
+    private char[][] picture;
 }
