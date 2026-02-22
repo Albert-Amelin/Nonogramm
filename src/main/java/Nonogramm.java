8fc4865063730c83cdec77202fa4d79e26652a3f
Albert Amelin <albert.amelin@gmx.de>
2026-02-22 20:45:03 +0100

    feat: Added Getter and Setter for Nonogramm Class.

--- src/main/java/Nonogramm.java
+++ src/main/java/Nonogramm.java
@@ -6,4 +6,44 @@ public class Nonogramm {
     private ArrayList<ArrayList<Integer>> rowSequences;
     private ArrayList<ArrayList<Integer>> colSequences;
     private char[][] picture;
+
+    public Nonogramm(ArrayList<ArrayList<Integer>> rowSequences,
+                     ArrayList<ArrayList<Integer>> colSequences,
+                     char[][] picture) {
+        this.rowSequences = rowSequences;
+        this.colSequences = colSequences;
+        this.picture = picture;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public ArrayList<ArrayList<Integer>> getRowSequences() {
+        return rowSequences;
+    }
+
+    public void setRowSequences(ArrayList<ArrayList<Integer>> rowSequences) {
+        this.rowSequences = rowSequences;
+    }
+
+    public ArrayList<ArrayList<Integer>> getColSequences() {
+        return colSequences;
+    }
+
+    public void setColSequences(ArrayList<ArrayList<Integer>> colSequences) {
+        this.colSequences = colSequences;
+    }
+
+    public char[][] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(char[][] picture) {
+        this.picture = picture;
+    }
 }
