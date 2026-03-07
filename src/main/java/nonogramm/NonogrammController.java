d4851bf25c2a461e6fc7acfaf34946b75e08d90e
Hannah Schlich <s-haschl@uni-greifswald.de>
2026-03-07 16:47:29 +0100

    add: trial Window

--- src/main/java/nonogramm/NonogrammController.java
+++ src/main/java/nonogramm/NonogrammController.java
@@ -1,4 +1,15 @@
 package nonogramm;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
 public class NonogrammController {
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonClick(){
+        System.out.println("Button clicked");
+    }
 }
